a31646ad880b446aed2bf7dda3ca59a282cd9193
David Okenla <116645288+Okenlathegithubber@users.noreply.github.com>
2024-03-12 18:25:49 +0100

    Update project_1.py

--- Week_1/project_1.py
+++ Week_1/project_1.py
@@ -1,21 +1,27 @@
-# This python program is used to generate Simple Interest, Compound Interest and Annuity Plan
+# This python program is used to generate Simple Interest, Compound Interest and Annuity Plan.
+
+# "time" is an in-built functon.
 import time
 
-# The Functions are used to calculate the and Annuity Plan along side the Simple and Compound Interest
+# The Functions are used to calculate the Annuity Plan along side the Simple and Compound Interest.
+# The Function SI calculates the simple interest
 def SI(p,r,t):
     a = p * (1 + ( r / 100 ) * t)
     return a
 
+# The Function CI calculates the simple interest
 def CI(p,r,n,t):
     power = n * t
     a = p * ( 1 + r / n ) ** power
     return a
 
+# The Function AP calculates the simple interest
 def AP(p,r,n,t):
     power = n * t
     a = p * ((( 1 + r / n ) ** power) - 1) / (r / n)
     return a
 
+# In the code below, the time function is used to slow the speed on how the print commands show.
 print("Welcome to the Interest Calculator\n")
 time.sleep(1.5)
 print("This program is calculating Simple Interest, Compound Interest and Annuity Plan\n")
@@ -27,13 +33,15 @@ time.sleep(1.5)
 print("To calculate Annuity Plan, Enter 3\n")
 time.sleep(1.5)
 
+# choice variable is used to get the user's input on what they want to calculate.
 choice = int(input("What do you wish to Calculate? >> "))
 
 if choice == 1:
     p = int(input("Enter initial principal balance >> "))
-    r =  int(input("Enter annual interest rate >> "))
+    r = int(input("Enter annual interest rate >> "))
     t = int(input("Enter time (in years) >> "))
 
+    # si is the variable used to call the function SI.
     si=SI(p,r,t)
     print(f"Final Amount = N{si}")
 
